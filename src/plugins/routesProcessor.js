c8c0fedd4f5b70ba8b826aec0807722943355ee8
MoffunW <moffuncan@gmail.com>
2024-11-06 10:36:13 +0300

    fix: fix redirect to 404 if not logged in; refactor

--- src/plugins/routesProcessor.js
+++ src/plugins/routesProcessor.js
@@ -4,26 +4,50 @@ import store from "@/store";
 import * as api from "@/api";
 import { TOKEN_KEY } from "@/config";
 
+function pathExists(path, routes) {
+  return routes.some(
+    x => x.path === `/:lang/${path}` || x.path === `/:lang/${path}/:token`
+  );
+}
+
+function checkIfUnAuthRoute(path, routes) {
+  return routes.some(
+    route =>
+      route.unAuth &&
+      (route.path === `/:lang/${path}` ||
+        route.path === `/:lang/${path}/:token`)
+  );
+}
+
 Vue.prototype.$checkRoute = async () => {
   if (!store.state.serverSettings) await api.getServerConfig();
+
   const user = Vue.prototype.$user;
-  const loggedIn = !!localStorage.getItem(TOKEN_KEY);
+  const loggedIn = Boolean(localStorage.getItem(TOKEN_KEY));
   const path = window.location.hash.split("/")[2];
-  const unAuth = !!router.options.routes
-    .filter(x => x.unAuth)
-    .find(
-      x => x.path === `/:lang/${path}` || x.path === `/:lang/${path}/:token`
-    );
+  const routes = router.options.routes;
+  const unAuth = checkIfUnAuthRoute(path, routes);
+
+  if (path === "404") return;
+
   if (!path) {
     user.go(loggedIn ? "dashboard" : "login");
     return;
   }
-  if (path && !loggedIn && path != "login" && !unAuth) {
-    if (path != "404") user.go("404");
+
+  if (!pathExists(path, routes)) {
+    user.go("404");
     return;
-  } else if (path && loggedIn && unAuth) {
-    if (path != "404") user.go("dashboard");
-    return;
+  }
+
+  if (loggedIn) {
+    if (unAuth) {
+      user.go("dashboard");
+      return;
+    }
+  } else {
+    if (path == "login" || unAuth) return;
+    user.go("login");
   }
 };
 
