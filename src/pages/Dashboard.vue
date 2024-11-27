6bbfc637718f49973484cb87a21c49581cd09901
MoffunW <moffuncan@gmail.com>
2024-11-27 10:42:43 +0300

    refactor: remove unused code

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -146,18 +146,6 @@
 </template>
 
 <script>
-/*function debounce(fn, delay) {
-  var timeoutID = null;
-  return function() {
-    clearTimeout(timeoutID);
-    var args = arguments;
-    var that = this;
-    timeoutID = setTimeout(function() {
-      fn.apply(that, args);
-    }, delay);
-  };
-}*/
-
 import axios from "axios";
 import VueGridLayout from "vue-grid-layout";
 
